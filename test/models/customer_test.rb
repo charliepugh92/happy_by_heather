a5ca6822bb068061cc48b98e51f983e53f31e1f3
Charlie <cpugh@evident.io>
2018-03-22 00:35:25 -0400

    Updated loofah gem to fix a security vulnerability

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -7,4 +7,18 @@ class CustomerTest < ActiveSupport::TestCase
     should validate_presence_of(:email)
     should validate_presence_of(:phone_number)
   end
+
+  context 'callbacks' do
+    should 'destroy orders and address when destroyed' do
+      order = create(:order)
+      customer = order.customer
+      address = create(:customer_address, customer: customer)
+
+      assert_difference 'CustomerAddress.count', -1 do
+        assert_difference 'Order.count', -1 do
+          customer.destroy
+        end
+      end
+    end
+  end
 end
